2350503aa0e99d49699cf3e63778b11585d98b32
jonik2909 <boronovjonibek@gmail.com>
2026-05-19 22:06:34 +0900

    feat: publish Job Categories component

--- src/screens/homePage/JobCategories.tsx
+++ src/screens/homePage/JobCategories.tsx
@@ -1,3 +1,37 @@
+import { Box, Container, Stack } from "@mui/material";
+
+const mockCategories = [
+  {
+    categoryName: "Web Development",
+    categoryImage: "/icons/web-development.svg",
+  },
+  {
+    categoryName: "Mobile Development",
+    categoryImage: "/icons/mobile-development.svg",
+  },
+  { categoryName: "UI/UX Designer", categoryImage: "/icons/ux-design.svg" },
+  { categoryName: "Data Science", categoryImage: "/icons/data-science.svg" },
+  { categoryName: "DevOps", categoryImage: "/icons/devops.svg" },
+  { categoryName: "Cybersecurity", categoryImage: "/icons/cyber-security.svg" },
+];
+
 export default function JobCategories() {
-  return <div>JOB CATEGORIES</div>;
+  return (
+    <Stack className="job-category">
+      <Container className="container">
+        <span className="title">Popular Job Categories</span>
+        <p className="desc">2020 jobs live - 293 added today.</p>
+        <Stack className="wrapper">
+          {mockCategories.map((category, index) => (
+            <Box className="category-card" key={index}>
+              <div className="icon-box">
+                <img src={category.categoryImage} alt={category.categoryName} />
+              </div>
+              <span className="cat-title">{category.categoryName}</span>
+            </Box>
+          ))}
+        </Stack>
+      </Container>
+    </Stack>
+  );
 }
