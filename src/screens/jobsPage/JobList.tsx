089ac0201b0461b1d356af1ab7d9f1ea3f4af56f
jonik2909 <boronovjonibek@gmail.com>
2026-05-21 15:31:55 +0900

    feat: get Jobs page job list

--- src/screens/jobsPage/JobList.tsx
+++ src/screens/jobsPage/JobList.tsx
@@ -11,12 +11,30 @@ import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import OtherHeader from "../../components/headers/OtherHeader";
 import Filter from "./Filter";
 import JobCard from "../../components/card/JobCard";
-import { useState } from "react";
-
-const jobs = [1, 2, 3, 4, 5, 6];
+import { useEffect, useState } from "react";
+import jobService from "../../services/JobService";
+import { JobSort } from "../../types/enums/job.enum";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { selectJobs, setJobs } from "./state";
+import type { Job } from "../../types/job";
 
 const JobList = () => {
+  const dispatch = useAppDispatch();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const jobs = useAppSelector(selectJobs);
+
+  useEffect(() => {
+    jobService
+      .getJobs({
+        page: 1,
+        limit: 6,
+        sort: JobSort.createdAt,
+      })
+      .then((data) => {
+        dispatch(setJobs(data));
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const sortingClickHandler = (e: any) => {
     setAnchorEl(e.currentTarget);
@@ -64,14 +82,14 @@ const JobList = () => {
             </Box>
           </Stack>
           <Stack className="wrapper">
-            {/* {jobs && jobs.length !== 0 ? (
-              jobs.map(() => <JobCard />)
+            {jobs && jobs.list.length !== 0 ? (
+              jobs.list.map((job: Job) => <JobCard job={job} />)
             ) : (
               <div className="no-data">
                 <InfoOutlinedIcon />
                 <span>No data found!</span>
               </div>
-            )} */}
+            )}
           </Stack>
           <Stack className="pagination-box">
             <Pagination color="primary" count={1} page={1} />
